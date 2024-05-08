e0ef45beb94af4c37bc999dae78f9b335d4965dd
LusineAm <lusine.a.amiraghyan@gmail.com>
2024-05-08 11:09:41 +0400

    add files

---
+++ README.md
@@ -0,0 +1,3 @@
+# async-race-api
+
+For more details and instructions about the server check the link https://github.com/mikhama/async-race-api.git
---
+++ db.js
@@ -0,0 +1,8 @@
+const garage = require("./data/garage");
+const winners = require("./data/winners");
+
+module.exports = {
+  garage,
+  winners,
+}
+
